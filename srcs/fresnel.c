de3e28f36a0387bd5e58d2c7c3f517d6ed647520
Ctsu27 <kehuang42student@gmail.com>
2018-11-26 12:37:14 +0100

    {update}: [raytracing] add obj absorption and NORME functions

--- srcs/fresnel.c
+++ srcs/fresnel.c
@@ -6,12 +6,21 @@
 /*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 14:02:33 by kehuang           #+#    #+#             */
-/*   Updated: 2018/11/16 14:03:10 by kehuang          ###   ########.fr       */
+/*   Updated: 2018/11/26 12:01:42 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "rt_ts.h"
+
+static void	swap_double(double *a, double *b)
+{
+	static double	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 double	fresnel(t_ray const ray, t_vec3 const inter, t_vec3 const normal,
 		double const ior)
@@ -24,22 +33,18 @@ double	fresnel(t_ray const ray, t_vec3 const inter, t_vec3 const normal,
 
 	view = sub_vec3(inter, ray.pos);
 	cos[0] = dot_vec3(normal, view);
+	eta[0] = ior;
+	eta[1] = 1.0;
 	if (cos[0] < 0)
-	{
-		eta[0] = 1.0;
-		eta[1] = ior;
-	}
-	else
-	{
-		eta[0] = ior;
-		eta[1] = 1.0;
-	}
+		swap_double(eta, eta + 1);
 	sin_t = (eta[0] / eta[1]) * fmax(0.0, 1.0 - cos[0] * cos[0]);
 	if (sin_t >= 1.0)
 		return (1.0);
 	cos[1] = fmax(0.0, 1.0 - sin_t * sin_t);
 	cos[0] = (cos[0] < 0.0) ? -cos[0] : cos[0];
-	r[0] = (eta[1] * cos[0] - eta[0] * cos[1]) / (eta[1] * cos[0] + eta[0] * cos[1]);
-	r[1] = (eta[0] * cos[0] - eta[1] * cos[1]) / (eta[0] * cos[0] + eta[1] * cos[1]);
+	r[0] = (eta[1] * cos[0] - eta[0] * cos[1])
+		/ (eta[1] * cos[0] + eta[0] * cos[1]);
+	r[1] = (eta[0] * cos[0] - eta[1] * cos[1])
+		/ (eta[0] * cos[0] + eta[1] * cos[1]);
 	return ((r[0] * r[0] + r[1] * r[1]) / 2);
 }
--- srcs/raytracing.c
+++ srcs/raytracing.c
@@ -6,46 +6,49 @@
 /*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 13:58:16 by kehuang           #+#    #+#             */
-/*   Updated: 2018/11/16 14:16:22 by kehuang          ###   ########.fr       */
+/*   Updated: 2018/11/26 11:57:26 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-static t_poly	*get_nearest_obj(t_rtv1 const *core, t_ray const ray,
-		double *distance)
+static t_inter	get_inter(t_rtv1 const *core, t_ray const ray)
 {
+	static t_inter	inter;
 	static t_poly	*ptr;
-	static t_poly	*near;
-	static double	tmp;
+	static double	t[2];
 
 	ptr = core->objs;
-	near = NULL;
+	inter.obj = NULL;
+	t[1] = 0xffffffff;
 	while (ptr != NULL)
 	{
-		if (core->inter_obj[ptr->type](ray, ptr->data, &tmp))
-		{
-			if (tmp > 0.01 && (near == NULL || tmp < *distance))
+		if (core->inter_obj[ptr->type](ray, ptr->data, &t[0]))
+			if (t[0] > 0.01 && (inter.obj == NULL || t[0] < t[1]))
 			{
-				*distance = tmp;
-				near = ptr;
+				t[1] = t[0];
+				inter.obj = ptr;
 			}
-		}
 		ptr = ptr->next;
 	}
-	return (near);
+	if (inter.obj != NULL)
+	{
+		inter.pos = add_vec3(ray.pos, mul_vec3(ray.dir, t[1]));
+		inter.normal = core->normal_obj[inter.obj->type](inter.obj->data,
+				(inter.obj->type != TYPE_PLANE) ? inter.pos : ray.dir);
+	}
+	return (inter);
 }
 
 t_clr	ray_trace_refraction(t_rtv1 const *core, t_ray ray,
-		t_vec3 const inter, t_vec3 const normal,
-		double const absorbance, unsigned int const rebound)
+		t_inter const inter, unsigned int const rebound)
 {
 	static t_vec3	view;
 	static double	tmp[3];
 	static double	ior = 1.5;
 
-	view = sub_vec3(inter, ray.pos);
-	tmp[0] = dot_vec3(normal, view);
+	view = sub_vec3(inter.pos, ray.pos);
+	tmp[0] = dot_vec3(inter.normal, view);
 	if (tmp[0] < 0)
 	{
 		tmp[0] = -tmp[0];
@@ -58,61 +61,53 @@ t_clr	ray_trace_refraction(t_rtv1 const *core, t_ray ray,
 		tmp[1] = 1.0;
 		tmp[2] = 1.0 - tmp[0] * tmp[0];
 	}
-	ray.pos = inter;
-	ray.dir = norm_vec3(add_vec3(mul_vec3(normal, tmp[1]),
-				mul_vec3(normal, tmp[1] * tmp[0] - sqrt(tmp[2]))));
-	return (mul_clr(raytrace(core, ray, rebound), 1.0 - absorbance));
+	ray.pos = inter.pos;
+	ray.dir = norm_vec3(add_vec3(mul_vec3(inter.normal, tmp[1]),
+				mul_vec3(inter.normal, tmp[1] * tmp[0] - sqrt(tmp[2]))));
+	return (mul_clr(raytrace(core, ray, rebound), 1.0 - inter.obj->absorption));
 }
 
 t_clr	ray_trace_reflection(t_rtv1 const *core, t_ray ray,
-		t_vec3 const inter, t_vec3 const normal,
-		double const absorbance, unsigned int const rebound)
+		t_inter const inter,  unsigned int const rebound)
 {
 	static t_vec3	view;
 
-	view = sub_vec3(ray.pos, inter);
-	ray.pos = inter;
-	ray.dir = norm_vec3(sub_vec3(mul_vec3(normal, 2.0 * dot_vec3(normal, view)),
-				view));
-	return (mul_clr(raytrace(core, ray, rebound), 1.0 - absorbance));
+	view = sub_vec3(ray.pos, inter.pos);
+	ray.pos = inter.pos;
+	ray.dir = norm_vec3(sub_vec3(mul_vec3(inter.normal,
+					2.0 * dot_vec3(inter.normal, view)), view));
+	return (mul_clr(raytrace(core, ray, rebound), 1.0 - inter.obj->absorption));
 }
 
 t_clr		ray_trace_fresnel(t_rtv1 const *core, t_ray ray,
-		t_vec3 const inter, t_vec3 const normal,
-		double const absorbance, unsigned int const rebound)
+		t_inter const inter, unsigned int const rebound)
 {
-	double	kr;
+	double	k;
 
-	kr = fresnel(ray, inter, normal, 1.5);
-	return (add_clr(mul_clr(ray_trace_reflection(core, ray, inter,
-						normal, 1.0 - absorbance, rebound), kr),
-				mul_clr(ray_trace_refraction(core, ray, inter,
-						normal, 1.0 - absorbance, rebound), 1.0 - kr)));
+	k = fresnel(ray, inter.pos, inter.normal, 1.5);
+	return (add_clr(mul_clr(ray_trace_reflection(core, ray, inter, rebound), k),
+				mul_clr(ray_trace_refraction(core, ray, inter, rebound),
+					1.0 - k)));
 }
 
 t_clr		raytrace(t_rtv1 const *core, t_ray ray, unsigned int const rebound)
 {
-	static t_clr	(*re_trace[3])(t_rtv1 const *, t_ray, t_vec3 const,
-			t_vec3 const, double const, unsigned int const) = {&ray_trace_reflection,
-		&ray_trace_refraction, &ray_trace_fresnel};
+	static t_clr	(*re_trace[3])(t_rtv1 const *, t_ray, t_inter const,
+			unsigned int const) = {&ray_trace_reflection, &ray_trace_refraction,
+		&ray_trace_fresnel};
+	static t_inter	inter;
 	t_clr			color_pxl;
-	t_poly			*obj_near;
-	t_vec3			inter;
-	t_vec3			normal;
-	double			distance;
-	static double	absorbance = 0.40;
 
-	color_pxl = new_clr(0.0, 0.0, 0.0, 0.0);
-	if ((obj_near = get_nearest_obj(core, ray, &distance)) != NULL)
+	inter = get_inter(core, ray);
+	if (inter.obj != NULL)
 	{
-		inter = add_vec3(ray.pos, mul_vec3(ray.dir, distance));
-		normal = core->normal_obj[obj_near->type](obj_near->data,
-				(obj_near->type != TYPE_PLANE) ? inter : ray.dir);
-		color_pxl = handle_color(core, normal, obj_near, inter);
-		if (obj_near->material != MATERIAL_DEFAULT && rebound > 0)
-			color_pxl = add_clr(mul_clr(color_pxl, absorbance),
-					re_trace[obj_near->material](core, ray, inter,
-						normal, absorbance, rebound - 1));
+		color_pxl = handle_color(core, inter.normal, inter.obj, inter.pos);
+		if (inter.obj->material != MATERIAL_DEFAULT && rebound > 0)
+			color_pxl = add_clr(mul_clr(color_pxl, inter.obj->absorption),
+					re_trace[inter.obj->material](core, ray, inter,
+						rebound - 1));
 	}
+	else
+		return (new_clr(0.0, 0.0, 0.0, 0.0));
 	return (color_pxl);
 }
