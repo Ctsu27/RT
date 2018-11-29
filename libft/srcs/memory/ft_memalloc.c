562540f709619649279953bbd27c2abf88f0c674
Ctsu27 <kehuang42student@gmail.com>
2018-11-29 13:51:55 +0100

    {fix}: [update] no more invalid write

--- libft/srcs/memory/ft_memalloc.c
+++ libft/srcs/memory/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/08 15:30:41 by kehuang           #+#    #+#             */
-/*   Updated: 2018/02/08 15:30:43 by kehuang          ###   ########.fr       */
+/*   Updated: 2018/11/28 11:46:05 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memalloc(size_t size)
 {
-	int		*dest;
+	char	*dest;
 
-	if ((dest = (int *)malloc(sizeof(*dest) * size)) == NULL)
+	if ((dest = (char *)malloc(size)) == NULL)
 		return (NULL);
 	while (size > 0)
 	{
 		dest[size - 1] = 0;
 		size--;
 	}
-	return (dest);
+	return ((void *)dest);
 }
