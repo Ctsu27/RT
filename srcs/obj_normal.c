147701d36fe5eeaa02651333ce6b5efaaf74bb3e
Ctsu27 <kehuang42student@gmail.com>
2018-12-05 17:07:54 +0100

    {fix}: [obj_normal] all normal was reversed

--- srcs/obj_normal.c
+++ srcs/obj_normal.c
@@ -6,7 +6,7 @@
 /*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 16:01:33 by kehuang           #+#    #+#             */
-/*   Updated: 2018/11/10 15:35:18 by kehuang          ###   ########.fr       */
+/*   Updated: 2018/12/05 17:07:20 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_vec3			normal_cylinder(void *data, t_vec3 intersect)
 	static t_vec3	tmp;
 
 	cyl = (t_cyl *)data;
-	normal = rot_vec3(sub_vec3(cyl->pos, intersect), cyl->rot);
+	normal = rot_vec3(sub_vec3(intersect, cyl->pos), cyl->rot);
 	normal.y = 0;
 	tmp.x = 360 - cyl->rot.x;
 	tmp.y = 0;
@@ -37,7 +37,7 @@ t_vec3			normal_cone(void *data, t_vec3 intersect)
 	static t_vec3	tmp;
 
 	cone = (t_cone *)data;
-	normal = rot_vec3(sub_vec3(cone->pos, intersect), cone->rot);
+	normal = rot_vec3(sub_vec3(intersect, cone->pos), cone->rot);
 	normal.y = -normal.y * tan(cone->radius * cone->radius);
 	tmp.x = 360 - cone->rot.x;
 	tmp.y = 360 - cone->rot.y;
@@ -52,7 +52,7 @@ t_vec3			normal_plane(void *data, t_vec3 ray_dir)
 	static t_vec3	normal;
 
 	plane = (t_plane *)data;
-	if (dot_vec3(ray_dir, plane->dir) < 0.0)
+	if (dot_vec3(ray_dir, plane->dir) > 0.0)
 		plane->dir = mul_vec3(plane->dir, -1);
 	normal = norm_vec3(plane->dir);
 	return (normal);
@@ -60,5 +60,5 @@ t_vec3			normal_plane(void *data, t_vec3 ray_dir)
 
 t_vec3			normal_sphere(void *data, t_vec3 intersect)
 {
-	return (norm_vec3(sub_vec3(((t_sphere *)data)->pos, intersect)));
+	return (norm_vec3(sub_vec3(intersect, ((t_sphere *)data)->pos)));
 }
